f9bb43b419fd620b298a90d8439cad85d7a8c38b
alvin <alvoausto@gmail.com>
2022-09-09 13:09:13 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+/**
+*This is a program to display single digit base ten numbers
+*aphabet in lowercase
+*/
+#include <stdio.h>
+/**
+*this is a program to display the alphabet in small letters
+*Main - standard IO used
+*void arguments
+*Return:0 on success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
