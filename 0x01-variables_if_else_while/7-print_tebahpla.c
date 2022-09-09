a02b3159e499e2e0d9bb40693d24083e53aece3f
alvin <alvoausto@gmail.com>
2022-09-09 13:20:22 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/**
+*This is a program to display single digit base ten numbers
+*aphabet in lowercase
+*/
+#include <stdio.h>
+/**
+*this is a program to display the alphabet in small letters
+*Main - standard IO used
+*void arguments
+*Return:0 on success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 'z' ; n <= 'a' ; n--)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
