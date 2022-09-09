3afc2c6f03ab3da19519958cbecf61abf134e35c
alvin <alvoausto@gmail.com>
2022-09-09 12:52:50 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/**
+*This is a program to display the
+*aphabet in lowercase
+*/
+#include <stdio.h>
+/**
+*this is a program to display the alphabet in small letters
+*Main - standard IO used
+*void arguments
+*Return:0 on success
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if !(ch == q || ch == e)
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
