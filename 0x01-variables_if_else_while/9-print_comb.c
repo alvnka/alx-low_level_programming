8b43c2b25d61fbfbc93437b07dfad47de88e1cf5
alvin <alvoausto@gmail.com>
2022-09-09 13:45:46 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+/**
+*This is a program to display single digit base ten numbers
+*aphabet in lowercase
+*/
+#include <stdio.h>
+/**
+*this is a program to display the alphabet in small letters
+*Main - standard IO used
+*void arguments
+*Return:0 on success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+	putchar(n);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
