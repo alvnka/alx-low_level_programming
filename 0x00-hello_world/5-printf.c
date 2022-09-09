84747b3f51791c95df8644f17b391af45173c293
alvin <alvoausto@gmail.com>
2022-09-08 19:58:11 -0700

    correction 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 /*
 this is the header file containing the printf function  
  */
-#include <stdio.h>;
+#include <stdio.h>
 int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,");
 }
