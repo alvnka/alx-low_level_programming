7a5cbc5f098677737e8c0254c6da22b2513b2edb
alvin <alvoausto@gmail.com>
2022-09-08 20:28:34 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	char myString=""Programming is like building a multilingual puzzle";
+	char myString="\"Programming is like building a multilingual puzzle";
 
 	puts(myString);
 
