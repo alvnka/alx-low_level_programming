1bd61d919c8d10e80744c4158d21d7f921498beb
alvin <alvoausto@gmail.com>
2022-09-13 06:07:33 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,12 @@ void jack_bauer(void)
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m <= 59; m++)
+		{
 		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0'
+		_putchar((i % 10) + '0');
 		_putchar(':');
 		_putchar((j / 10) + '0');
 		_putchar((j % 10) + '0');
+		}
 	}
 }
