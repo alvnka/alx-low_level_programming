945bcc456623bebede5c22bd80b1049c391742f1
alvin <alvoausto@gmail.com>
2022-09-13 06:40:53 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,13 +8,12 @@
 
 int _islower(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-	return (0);
-	}
-	else if (c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 	return (1);
 	}
-	return (404);
+	else
+	{
+	return (0)
+	}
 }
