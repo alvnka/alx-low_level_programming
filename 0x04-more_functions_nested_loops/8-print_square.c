bc56fed1c69e991a2bccafe7705b4d0d9c927609
alvin <alvoausto@gmail.com>
2022-09-15 14:13:56 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_square - prints an equivalent
+ * number of columns as rows
+ * @size: number of columns or rows
+ *
+ * Return: square
+ */
+
+void print_square(int size)
+{
+	int i = 0, j = 0, k = size;
+	
+	if ( size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	while (i < size)
+	{
+		for (j = 0; j < k; j++)
+		{
+		_putchar('#');
+		}
+		_putchar('\n');
+		i++;
+	}
+	}
+}
