619a57cad53ce760cad5d2a7d59233bab5695877
alvin <alvoausto@gmail.com>
2022-09-19 13:55:28 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a:pointer to a's address
+ * @b:pointer to b's address
+ */
+void swap_int(int *a, int *b)
+{
+	int tempLocation
+
+	tempLocation = *a
+	* a = *b
+	* b = tempLocation
+}
