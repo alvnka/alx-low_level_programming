400719350b60854a04d705bd6e7673a941410f0f
alvin <alvoausto@gmail.com>
2022-09-19 19:35:57 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: holds the string to be printed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int a = 0, i = 0
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (i = a - 1; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
