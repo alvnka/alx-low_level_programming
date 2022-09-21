3535569b5fcdc48dd94af7ddb66063310147c10d
alvin <alvoausto@gmail.com>
2022-09-21 10:48:21 -0700

    testing

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncpy - a function to copy second
+ * parameter strings to first
+ * @dest: holds beginning of intended string
+ * @src: is the string to be added to *dest
+ * @n: max bytes *src can have
+ * Return: pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+/**
+	while (dest[i] != '\0')
+	{
+		++i;
+	}
+*/
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		if (j < n)
+		{
+		dest[i] = src[j];
+		}
+		i++;
+	}
+	if (j != 0)
+	{
+	dest[i] = '\0';
+	}
+	return (dest);
+}
