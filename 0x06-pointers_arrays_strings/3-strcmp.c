c8d9d6a980b5e09b3db9be179c9902b68b04d0b4
alvin <alvoausto@gmail.com>
2022-09-21 14:26:49 -0700

    3-strcmp.c corrected

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{	
-		if(s1[i] <= s2 [i])
-		{
-			j = j - 1;
-		}
-		if(s1[i] >= s2[i])
-		{
-			j = j + 1;
-		}
-		if(s1[i] == s2 [i])
-		{
-			j = j + 0;
-		}
+	while (j == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		j = *(s1 + i) - *(s2 + i);
 		i++;
 	}
+
 	return (j);
 }
