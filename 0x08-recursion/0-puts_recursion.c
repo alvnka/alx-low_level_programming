a4c8fbb2df4f16b3465257b638ae92186c107a50
alvin <alvoausto@gmail.com>
2022-09-28 08:39:58 -0700

    0-puts_recursion.c working well and betty compliant

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string and new line
+ * @s: string to be printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+	return;
+	}
+	_putchar(*(s + i));
+	i++;
+	_puts_recursion((s + i));
+}
