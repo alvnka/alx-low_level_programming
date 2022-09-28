d4f976f393372d26a8990cb1ea012aed1076e7e1
alvin <alvoausto@gmail.com>
2022-09-28 16:14:15 -0700

    0-puts_recursion.c corrected and working well and betty compliant

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,16 +9,13 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] == '\0')
-	{
-		return;
-	}
+	if (s[i] != '\0')
 	{
 	_putchar(*(s + i));
 	i++;
 	_puts_recursion((s + i));
 	}
-	if (s[i] == '\0' || (s + i) == NULL)
+	else
 	{
 		_putchar('\n');
 	}
