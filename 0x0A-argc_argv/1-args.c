e7f12d0e751fe0830d7956a700e32778a6383b00
alvin <alvoausto@gmail.com>
2022-09-30 08:19:00 -0700

    unused variables cleared

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,6 +7,13 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return(0);
+	int i = 0;
+
+	(void) argv;
+	while (i < argc)
+	{
+		i++;
+	}
+	printf("%d", i);
+	return (0);
 }
