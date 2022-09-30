ac563f7049ceb9c8f254623a22503cd918e715b3
alvin <alvoausto@gmail.com>
2022-09-30 09:17:15 -0700

    3 done and betty compliant

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
