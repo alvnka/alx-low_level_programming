6ba53b272c32e0c2c0b30a5d63aa78522895f3bf
alvin <alvoausto@gmail.com>
2022-10-04 13:37:41 -0700

    2-str_concat.c done and tested

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,8 +32,11 @@ int **alloc_grid(int width, int height)
 		if (point_it[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
+			{
+				free(point_it);
+				return (NULL);
+			}
 			free(point_it);
-			return (NULL);
 		}
 	}
 	for (i = 0; i < height; i++)
@@ -44,5 +47,5 @@ int **alloc_grid(int width, int height)
 		}
 	}
 	return (point_it);
-	free (point_it);
+	free(point_it);
 }
