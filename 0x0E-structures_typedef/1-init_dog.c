2b8cfb73ba928ffe05bcb389abc16a09c1922801
alvin <alvoausto@gmail.com>
2022-10-11 12:41:03 -0700

    task 1 done and tested

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * init_dog - function to initialize struct dog
+ * @d: - element 1
+ * @name: element 2
+ * @age: element 3
+ * @owner:element 4
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
