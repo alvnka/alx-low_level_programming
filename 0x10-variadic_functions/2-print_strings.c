489a59734fbd7fdb44eefa565dc6940b0f4cf48a
alvin <alvoausto@gmail.com>
2022-10-14 10:25:59 -0700

    task 2 done and corrected

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		check = va_arg(no, char *);
-		if (check == NULL)
+		if (*check == '\0')
 		{
 			printf("nil");
 		}
