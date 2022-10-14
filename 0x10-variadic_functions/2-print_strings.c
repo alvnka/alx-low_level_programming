3a9ff7ffbf190c64b44f3ce32864557200fe52b8
alvin <alvoausto@gmail.com>
2022-10-14 10:07:50 -0700

    task 2 done and tested

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - function to print strings
+ * @separator: string that seperates them
+ * @n: number of strings to be printed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int  i;
+	char *check;
+
+	va_list(no);
+	va_start(no, n);
+	for (i = 0; i < n; i++)
+	{
+		check = va_arg(no, char *);
+		if (check == NULL)
+                 {
+                         printf("nil");
+                 }
+		else
+		{
+			printf("%s",check);
+		}
+		if ((i != (n - 1)) && (separator != NULL))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(no);
+}
