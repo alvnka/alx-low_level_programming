e15f14339c2c763d07e7d8dd5ab4b2cc1d4e1842
alvin <alvoausto@gmail.com>
2022-10-24 09:01:36 -0700

    task 0 and header file changes commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,22 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 
+/**
+ * struct list_linked - linked list required
+ * @str: holsds string
+ * @len: string lenght
+ * @next: next node
+ * description: this is the structure of
+ * a singly linked list
+ */
+typedef struct list_linked
+{
+	char *str;
+	unsigned int len;
+	struct list_linked *next;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
