a51c60ce7e3964daae7ec683377d68fb8d2599a0
alvin <alvoausto@gmail.com>
2022-11-02 15:00:22 -0700

    task 1 corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	for (letters = 0; text_content[letters] != '\0'; letters++)
+	for (letters = 0; text_content[letters]; letters++)
 	{
 		;
 	}
