0d34cbe4ce8aa89904233176c7d606409de4bb8f
alvin <alvoausto@gmail.com>
2022-11-02 14:32:15 -0700

    task 1 done and corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,8 +33,7 @@ int create_file(const char *filename, char *text_content)
 	for (letters = 0; text_content[letters] != '\0'; letters++)
 	{
 		;
-	}
-	letters = letters + 1;
+	}	
 
 	rwr = write(fld, text_content, letters);
 
