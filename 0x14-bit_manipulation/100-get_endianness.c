746676b36dadf32ac840a1866dcb42e3494821f2
alvnka <alvondu@gmail.com>
2023-07-07 00:49:07 +0300

    Correction to 100 (endianess)

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,14 +3,17 @@
  * get_endianness - function to ckeck endianness
  * @void: parameter
  * Return: return 0 if big endian and 1 if little endian.
-*/
-int get_endianness(void){
+ */
+int get_endianness(void)
+{
     unsigned int i = 1;
-    char *c = (char*)&i;
-    if (*c){
+    char *c = (char *)&i;
+    if (*c)
+    {
         return 1;
     }
-    else {
+    else
+    {
         return 0;
     };
 }
