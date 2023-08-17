a2b9564c457929573aa57e209854cb6428bdd296
alvnka <alvondu@gmail.com>
2023-08-18 00:53:34 +0300

    0x17 correction 8.1 and 7.1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,48 +10,39 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *previous_node, *current_node = *h;
+	dlistint_t *new_node, *previous_node, *current_node = *h;
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
+
+	new_node->prev = NULL;
+	new_node->n = n;
 
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->n = n;
 		new_node->next = current_node;
-		if (current_node != NULL)
+		if (current_node)
 			current_node->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
 
-	while (current_node != NULL && count < idx)
+	while (current_node && count < idx)
 	{
 		previous_node = current_node;
 		current_node = current_node->next;
 		count++;
 	}
 
-	if (current_node == NULL && count == idx)
-	{
-		previous_node->next = new_node;
-		new_node->prev = previous_node;
-		new_node->n = n;
-		new_node->next = NULL;
-		return (new_node);
-	}
-
 	if (count == idx)
 	{
+		new_node->next = current_node;
+		if (current_node)
+			current_node->prev = new_node;
 		previous_node->next = new_node;
 		new_node->prev = previous_node;
-		new_node->n = n;
-		new_node->next = current_node;
-		if (current_node != NULL)
-			current_node->prev = new_node;
 		return (new_node);
 	}
 
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,11 +9,33 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current_node = *head, *previous_node, *next_node;
+	dlistint_t *current_node = *head;
 	unsigned int count = 0;
 
-	if (*head)
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
 	{
-		
+		*head = current_node->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(current_node);
+		return (1);
 	}
+
+	while (current_node != NULL && count < index)
+	{
+		current_node = current_node->next;
+		count++;
+	}
+
+	if (current_node == NULL)
+		return (-1);
+
+	current_node->prev->next = current_node->next;
+	if (current_node->next)
+		current_node->next->prev = current_node->prev;
+	free(current_node);
+	return (1);
 }
